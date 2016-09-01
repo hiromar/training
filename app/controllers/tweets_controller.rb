74f839d1a91f0429a2b4dc96aa20eb2aa1b82f51
Hiromasa Iizuka <mabochin-3@docomo.ne.jp>
2016-09-01 21:38:52 +0900

    commit

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,18 +3,21 @@ class TweetsController < ApplicationController
 
 
   def index
+    @tweets = Tweet.all
   end
 
   def new
+    @tweet = Tweet.new
   end
 
   def create
     Tweet.create(tweet_params)
+    redirect_to controller: :tweets, action: :index
   end
 
   private
   def tweet_params
-    params.permit(:nickname, :image, :text)
+    params.require(:tweet).permit(:nickname, :image, :text)
   end
 
 end
--- db/migrate/20160822061955_create_tweets.rb
+++ db/migrate/20160822061955_create_tweets.rb
@@ -3,6 +3,7 @@ class CreateTweets < ActiveRecord::Migration
     create_table :tweets do |t|
       t.text :image
       t.text :text
+      t.string :nickname
       t.timestamps
     end
   end
